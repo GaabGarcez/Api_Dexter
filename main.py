957bfd6c8ef4ce5d24858c988fc1c3f4bb61f07b
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-10-11 14:47:30 -0300

    Última atualização com token, novo.

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Depends
 from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
-VERIFY_TOKEN = "EABjyeHmZA4e4BOzjLT0U0AgVRSyTt7P1qcQMT2IoTGCFdqHQP4sNYLnPyf8NmXBCJNIvJ6VMeDTpEGejNaUO5iFsa3ZAOtSLx0EhUHHuPOh6zTke6mUNZAoPeYQ8MYHyx5Pu8OGRDMZB4lo5y6m2oy8nno6lPk688XmuYLCHMB5DO34Ow2v3lqcUMBa28IVW"  # Substitua pelo seu token de verificação
-
-class WhatsAppMessage(BaseModel):
-    from_: str
-    text: dict
+class Message(BaseModel):
+    messages: list
 
 @app.get("/webhook")
 async def verify_webhook(hub_mode: str, hub_challenge: str, hub_verify_token: str):
-    if hub_verify_token == VERIFY_TOKEN:
+    VERIFY_TOKEN = "YOUR_VERIFY_TOKEN"  # Defina seu token de verificação aqui
+    if hub_mode == "subscribe" and hub_verify_token == VERIFY_TOKEN:
         return {"hub.challenge": hub_challenge}
     else:
-        raise HTTPException(status_code=403, detail="Token de verificação incorreto")
+        raise HTTPException(status_code=403, detail="Verification token mismatch")
 
 @app.post("/webhook")
-async def receive_whatsapp_message(request: Request):
-    data = await request.json()
-    messages = data.get("messages", [])
-    for message in messages:
+async def receive_whatsapp_message(data: Message):
+    for message in data.messages:
         sender_id = message.get("from")
         text = message.get("text", {}).get("body", "")
         print(f"Received message from {sender_id}: {text}")
 
-    # Aqui, você pode adicionar a lógica para enviar uma resposta usando a API do WhatsApp Business.
-    # Por simplicidade, estou apenas retornando a mensagem de resposta, mas na prática, você faria uma chamada API para enviar a mensagem.
     return {
         "messages": [
             {
@@ -37,4 +32,5 @@ async def receive_whatsapp_message(request: Request):
             }
         ]
     }
+
 
