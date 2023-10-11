5b1c53cd914d8ae6b0aa1a0d2f2427b84212119e
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-10-11 15:49:16 -0300

    Código novo

--- main.py
+++ main.py
@@ -1,50 +1,25 @@
-from fastapi import FastAPI, Request, HTTPException, Depends
-from pydantic import BaseModel
-import uvicorn
+from fastapi import FastAPI, Request, HTTPException
 
 app = FastAPI()
 
-class Message(BaseModel):
-    messages: list
-
-class ErrorNotification(BaseModel):
-    errors: list
-
-@app.get("/webhook")
-async def verify_webhook(hub_mode: str, hub_challenge: int, hub_verify_token: str):
-    VERIFY_TOKEN = "EABjyeHmZA4e4BOzjLT0U0AgVRSyTt7P1qcQMT2IoTGCFdqHQP4sNYLnPyf8NmXBCJNIvJ6VMeDTpEGejNaUO5iFsa3ZAOtSLx0EhUHHuPOh6zTke6mUNZAoPeYQ8MYHyx5Pu8OGRDMZB4lo5y6m2oy8nno6lPk688XmuYLCHMB5DO34Ow2v3lqcUMBa28IVW"  # Substitua por seu token de verificação
-    if hub_mode == "subscribe" and hub_verify_token == VERIFY_TOKEN:
-        return {"hub.challenge": hub_challenge}
-    else:
-        raise HTTPException(status_code=403, detail="Verification token mismatch")
+VERIFY_TOKEN = "SEU_TOKEN_DE_VERIFICAÇÃO"
 
 @app.post("/webhook")
-async def receive_whatsapp_message(request: Request):
-    try:
-        body = await request.json()
-        if "messages" in body:
-            data = Message(**body)
-            for message in data.messages:
-                sender_id = message.get("from")
-                text = message.get("text", {}).get("body", "")
-                print(f"Received message from {sender_id}: {text}")
-            return {
-                "messages": [
-                    {
-                        "to": sender_id,
-                        "content": {
-                            "text": "Obrigado por enviar a mensagem. O Dexter está ficando brabo!"
-                        }
-                    }
-                ]
-            }
-        elif "errors" in body:
-            error_data = ErrorNotification(**body)
-            for error in error_data.errors:
-                print(f"Error Code: {error['code']}, Title: {error['title']}, Details: {error['details']}")
-            return {"status": "Error received and logged"}
-        else:
-            return {"status": "Unknown payload received"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+async def webhook(request: Request):
+    data = await request.json()
+    # Processar a notificação do evento aqui
+    return {"status": "success"}
 
+@app.get("/webhook")
+async def verify(request: Request):
+    mode = request.query_params.get("hub.mode")
+    token = request.query_params.get("hub.verify_token")
+    challenge = request.query_params.get("hub.challenge")
+
+    if mode and token:
+        if mode == "subscribe" and token == VERIFY_TOKEN:
+            return {"hub.challenge": challenge}
+        else:
+            raise HTTPException(status_code=403, detail="Forbidden")
+    else:
+        raise HTTPException(status_code=400, detail="Bad Request")
