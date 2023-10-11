9693c633ba0bde9a2f243e0a618f5f8890444add
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-10-11 13:55:51 -0300

    Última atualização

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-# whatsapp_server.py
-
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
-VERIFY_TOKEN = "SeuTokenDeVerificação"  # Substitua por seu token de verificação real
+VERIFY_TOKEN = "YOUR_VERIFY_TOKEN"  # Substitua pelo seu token de verificação
 
-class Message(BaseModel):
-    messages: list
+class WhatsAppMessage(BaseModel):
+    from_: str
+    text: dict
 
 @app.get("/webhook")
 async def verify_webhook(hub_mode: str, hub_challenge: str, hub_verify_token: str):
     if hub_verify_token == VERIFY_TOKEN:
-        return {"hub.challenge": int(hub_challenge)}
+        return {"hub.challenge": hub_challenge}
     else:
         raise HTTPException(status_code=403, detail="Token de verificação incorreto")
 
 @app.post("/webhook")
-async def receive_whatsapp_message(data: Message):
-    # Processar as mensagens recebidas
-    for message in data.messages:
+async def receive_whatsapp_message(request: Request):
+    data = await request.json()
+    messages = data.get("messages", [])
+    for message in messages:
         sender_id = message.get("from")
         text = message.get("text", {}).get("body", "")
         print(f"Received message from {sender_id}: {text}")
@@ -37,3 +37,4 @@ async def receive_whatsapp_message(data: Message):
             }
         ]
     }
+
