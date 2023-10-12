50508dfe37f2763d5aa000f730f4c66d7cf51eb6
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-10-12 18:40:57 -0300

    Novo websokets

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 import requests
+import asyncio  # Adicionado importação de asyncio
 
 app = FastAPI()
 
-# Tabela de associação para mapear UUID para conexão WebSocket
+# Tabela de associação para mapear UUID para conexão WebSocket e mensagens pendentes
 connections = {}
+pending_messages = {}
 
 class Message(BaseModel):
     numero: str
     mensagem: str
 
-async def handle_websocket_messages(websocket: WebSocket):
+async def handle_websocket_messages(websocket: WebSocket, uuid: str):
     try:
         while True:
-            data = await websocket.receive_text()
-            # Aqui você pode processar a mensagem recebida se necessário
+            if uuid in pending_messages:
+                message = pending_messages.pop(uuid)
+                await websocket.send_text(message)
+            else:
+                await asyncio.sleep(1)  # Adicione um pequeno atraso para evitar uso excessivo de CPU
     except:
         pass
 
@@ -23,7 +28,7 @@ async def handle_websocket_messages(websocket: WebSocket):
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
     connections[uuid] = websocket
-    await handle_websocket_messages(websocket)
+    await handle_websocket_messages(websocket, uuid)
     del connections[uuid]
 
 @app.post("/webhook/")
@@ -39,9 +44,9 @@ async def read_webhook(message: Message):
 
     # Verifique se temos uma conexão WebSocket para esse UUID
     if uuid in connections:
-        websocket = connections[uuid]
-        await websocket.send_text(message.mensagem)
-        response = await websocket.receive_text()
-        return {"response": f"{response}"}
+        pending_messages[uuid] = message.mensagem
+        # Aguarde um pouco para dar tempo à coroutine handle_websocket_messages de processar a mensagem
+        await asyncio.sleep(2)
+        return {"response": "Mensagem enviada com sucesso!"}
     else:
         return {"response": "O dexter não está sendo executado no seu servidor."}
