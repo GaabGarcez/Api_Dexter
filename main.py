9a258ccebd4bcd85e072531ea0d0735db420e738
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-16 13:06:01 -0300

    Versão 1.0.5

--- main.py
+++ main.py
@@ -13,30 +13,38 @@ class Message(BaseModel):
     uuid_user: str
     mensagem: str
 
-async def process_message(websocket: WebSocket, uuid: str):
+async def enqueue_message(target_uuid: str, message: str):
+    message_id = str(uuid.uuid4())
+    if target_uuid not in message_queues:
+        message_queues[target_uuid] = asyncio.Queue()
+    await message_queues[target_uuid].put((message_id, message))
+    return message_id
+
+async def process_messages():
     while True:
-        if uuid in message_queues and not message_queues[uuid].empty():
-            message_id, message = await message_queues[uuid].get()
-            await websocket.send_text(message)
-            response = await websocket.receive_text()
-            responses[message_id] = response
-        await asyncio.sleep(0.1)  # Verifica regularmente por novas mensagens
+        for uuid, queue in message_queues.items():
+            if not queue.empty() and uuid in connections:
+                message_id, message = await queue.get()
+                websocket = connections[uuid]
+                await websocket.send_text(message)
+                response = await websocket.receive_text()
+                responses[message_id] = response
+        await asyncio.sleep(0.1)
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(process_messages())
 
 @app.websocket("/connect/{uuid}")
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
     connections[uuid] = websocket
-    if uuid not in message_queues:
-        message_queues[uuid] = asyncio.Queue()
-    asyncio.create_task(process_message(websocket, uuid))
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
     target_uuid = message.uuid_user
     if target_uuid in connections:
-        message_id = str(uuid.uuid4())
-        await message_queues[target_uuid].put((message_id, message.mensagem))
-        # Aguarda ativamente pela resposta
+        message_id = await enqueue_message(target_uuid, message.mensagem)
         while message_id not in responses:
             await asyncio.sleep(0.1)
         return {"response": responses.pop(message_id)}
