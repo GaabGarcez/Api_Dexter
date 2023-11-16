672de028af3c8050beb2ad4c09b069e30125d37a
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-16 09:43:59 -0300

    Alteração no futures

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 connections = {}
 message_queues = {}
-response_futures = {}
+response_callbacks = {}  # Usando callbacks para respostas
 
 class Message(BaseModel):
     uuid_user: str
@@ -16,7 +16,8 @@ class Message(BaseModel):
 async def send_message(websocket: WebSocket, message: str, message_id: str):
     await websocket.send_text(message)
     response = await websocket.receive_text()
-    response_futures[message_id].set_result(response)
+    if message_id in response_callbacks:
+        response_callbacks[message_id](response)
 
 @app.websocket("/connect/{uuid}")
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
@@ -27,7 +28,7 @@ async def websocket_endpoint(websocket: WebSocket, uuid: str):
     while True:
         if not message_queues[uuid].empty():
             message_id, message = await message_queues[uuid].get()
-            response_futures[message_id] = asyncio.Future()
+            response_callbacks[message_id] = lambda response: response_callbacks.pop(message_id, None)
             await send_message(websocket, message, message_id)
         else:
             await asyncio.sleep(0.1)
@@ -37,9 +38,9 @@ async def read_webhook(message: Message):
     target_uuid = message.uuid_user
     unique_message_id = str(uuid.uuid4())
     if target_uuid in connections:
+        response_future = asyncio.Future()
+        response_callbacks[unique_message_id] = response_future.set_result
         await message_queues[target_uuid].put((unique_message_id, message.mensagem))
-        response_future = response_futures[unique_message_id] = asyncio.Future()
-        await asyncio.sleep(0.1)  # Dá tempo para a mensagem ser enviada antes de aguardar a resposta
         response = await response_future
         return {"response": response}
     else:
