946294e46b2652f1fab4e304136a4a9d8d7e74ec
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-16 12:15:19 -0300

    Versão 1.0.2

--- main.py
+++ main.py
@@ -15,26 +15,27 @@ class Message(BaseModel):
 
 async def process_message(websocket: WebSocket, uuid: str):
     while True:
-        if not message_queues[uuid].empty():
+        if uuid in message_queues and not message_queues[uuid].empty():
             message_id, message = await message_queues[uuid].get()
             await websocket.send_text(message)
             response = await websocket.receive_text()
             responses[message_id] = response
         else:
-            await asyncio.sleep(0.1)  # Adiciona uma pequena pausa
+            await asyncio.sleep(0.1)
 
 @app.websocket("/connect/{uuid}")
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
     connections[uuid] = websocket
-    message_queues[uuid] = asyncio.Queue()
+    if uuid not in message_queues:
+        message_queues[uuid] = asyncio.Queue()
     asyncio.create_task(process_message(websocket, uuid))
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
     target_uuid = message.uuid_user
-    message_id = str(uuid.uuid4())
     if target_uuid in connections:
+        message_id = str(uuid.uuid4())
         await message_queues[target_uuid].put((message_id, message.mensagem))
         while message_id not in responses:
             await asyncio.sleep(0.1)
