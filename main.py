17808afd380313f8348bf1b500f516ea5cf3302b
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-15 21:24:11 -0300

    Código antigo

--- main.py
+++ main.py
@@ -1,61 +1,55 @@
 from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
-import asyncio
-from collections import defaultdict
-import uuid
+import requests
+import asyncio  # Adicionado importação de asyncio
 
 app = FastAPI()
 
+# Tabela de associação para mapear UUID para conexão WebSocket e mensagens pendentes
 connections = {}
-message_queues = defaultdict(asyncio.Queue)
-response_futures = {}  # Usando Futures para gerenciar respostas
+pending_messages = {}
 
 class Message(BaseModel):
     uuid_user: str
     mensagem: str
 
-async def handle_websocket_messages(websocket: WebSocket, uuid_str: str):
+async def handle_websocket_messages(websocket: WebSocket, uuid: str):
     try:
         while True:
-            if not message_queues[uuid_str].empty():
-                message_id, message = await message_queues[uuid_str].get()
+            if uuid in pending_messages:
+                message = pending_messages.pop(uuid)
                 await websocket.send_text(message)
-                response_futures[message_id] = asyncio.Future()
-                response = await asyncio.wait_for(response_futures[message_id], timeout=30)
-                message_queues[uuid_str].task_done()
             else:
-                await asyncio.sleep(0.1)
-    except asyncio.TimeoutError:
-        print(f"Timeout na resposta para UUID {uuid_str}")
-    except Exception as e:
-        print(f"Erro ao lidar com mensagens WebSocket: {e}")
-
-async def enqueue_message(uuid_str, message):
-    message_id = str(uuid.uuid4())  # Gera um identificador único para a mensagem
-    await message_queues[uuid_str].put((message_id, message))
-    return message_id
+                await asyncio.sleep(1)  # Adicione um pequeno atraso para evitgit addar uso excessivo de CPU
+    except:
+        pass
 
 @app.websocket("/connect/{uuid}")
-async def websocket_endpoint(websocket: WebSocket, uuid_str: str):
+async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
-    connections[uuid_str] = websocket
+    connections[uuid] = websocket
     try:
-        await handle_websocket_messages(websocket, uuid_str)
+        await handle_websocket_messages(websocket, uuid)
     except Exception as e:
         print(f"Erro ao lidar com mensagens WebSocket: {e}")
     finally:
-        del connections[uuid_str]
+        del connections[uuid]
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
-    uuid_str = message.uuid_user
-    if uuid_str in connections:
+    
+    
+    uuid = message.uuid_user  # O Xano retorna o UUID diretamente
+
+    # Verifique se temos uma conexão WebSocket para esse UUID
+    if uuid in connections:
+        websocket = connections[uuid]
         try:
-            message_id = await enqueue_message(uuid_str, message.mensagem)
-            return {"response": await asyncio.wait_for(response_futures.pop(message_id, None), timeout=30)}
-        except asyncio.TimeoutError:
-            return {"response": "Timeout esperando resposta do servidor local."}
+            await websocket.send_text(message.mensagem)  # Envie a mensagem para o servidor local
+            response_message = await websocket.receive_text()  # Aguarde a resposta do servidor local
+            return {"response": response_message}
         except Exception as e:
-            return {"response": f"Erro ao processar a mensagem: {e}"}
+            # Se ocorrer uma exceção ao tentar enviar/receber uma mensagem, assumimos que a conexão foi fechada
+            return {"response": "O Dexter não está sendo executado no seu servidor."}
     else:
-        return {"response": "UUID não encontrado ou conexão não estabelecida"}
+        return {"response": "O Dexter não está sendo executado no seu servidor."}
