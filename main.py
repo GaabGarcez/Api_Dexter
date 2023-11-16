f032a1850c6a07559bc3fe60bbcd657b6db78d94
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-16 12:40:45 -0300

    Versão 1.0.4

--- main.py
+++ main.py
@@ -8,42 +8,35 @@ app = FastAPI()
 connections = {}
 message_queues = {}
 responses = {}
-processing = set()  # Conjunto para rastrear quais UUIDs estão processando mensagens
 
 class Message(BaseModel):
     uuid_user: str
     mensagem: str
 
-async def enqueue_message(target_uuid: str, message: str):
-    message_id = str(uuid.uuid4())
-    if target_uuid not in message_queues:
-        message_queues[target_uuid] = asyncio.Queue()
-    await message_queues[target_uuid].put((message_id, message))
-    return message_id
-
-async def process_next_message(target_uuid: str):
-    if target_uuid in connections and not message_queues[target_uuid].empty() and target_uuid not in processing:
-        processing.add(target_uuid)
-        websocket = connections[target_uuid]
-        message_id, message = await message_queues[target_uuid].get()
-        await websocket.send_text(message)
-        response = await websocket.receive_text()
-        responses[message_id] = response
-        processing.remove(target_uuid)
+async def process_message(websocket: WebSocket, uuid: str):
+    while True:
+        if uuid in message_queues and not message_queues[uuid].empty():
+            message_id, message = await message_queues[uuid].get()
+            await websocket.send_text(message)
+            response = await websocket.receive_text()
+            responses[message_id] = response
+        await asyncio.sleep(0.1)  # Verifica regularmente por novas mensagens
 
 @app.websocket("/connect/{uuid}")
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
     connections[uuid] = websocket
-    # Processa mensagens assim que a conexão é estabelecida
-    asyncio.create_task(process_next_message(uuid))
+    if uuid not in message_queues:
+        message_queues[uuid] = asyncio.Queue()
+    asyncio.create_task(process_message(websocket, uuid))
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
     target_uuid = message.uuid_user
     if target_uuid in connections:
-        message_id = await enqueue_message(target_uuid, message.mensagem)
-        asyncio.create_task(process_next_message(target_uuid))
+        message_id = str(uuid.uuid4())
+        await message_queues[target_uuid].put((message_id, message.mensagem))
+        # Aguarda ativamente pela resposta
         while message_id not in responses:
             await asyncio.sleep(0.1)
         return {"response": responses.pop(message_id)}
