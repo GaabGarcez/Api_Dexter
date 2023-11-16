a229f6d7e0e92ca553ad17509ab73dccec3b13f2
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-15 22:35:03 -0300

    Nova conexão local

--- main.py
+++ main.py
@@ -1,55 +1,51 @@
 from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
-import requests
-import asyncio  # Adicionado importação de asyncio
+import asyncio
+from collections import defaultdict
 
 app = FastAPI()
 
-# Tabela de associação para mapear UUID para conexão WebSocket e mensagens pendentes
 connections = {}
-pending_messages = {}
+message_queues = defaultdict(asyncio.Queue)
 
 class Message(BaseModel):
     uuid_user: str
     mensagem: str
 
 async def handle_websocket_messages(websocket: WebSocket, uuid: str):
-    try:
-        while True:
-            if uuid in pending_messages:
-                message = pending_messages.pop(uuid)
+    while True:
+        try:
+            if not message_queues[uuid].empty():
+                message = await message_queues[uuid].get()
                 await websocket.send_text(message)
             else:
-                await asyncio.sleep(1)  # Adicione um pequeno atraso para evitgit addar uso excessivo de CPU
-    except:
-        pass
+                await asyncio.sleep(0.1)  # Reduzindo o atraso para melhor responsividade
+        except asyncio.CancelledError:
+            break  # Encerrando a loop corretamente na desconexão
+        except Exception as e:
+            print(f"Erro com WebSocket {uuid}: {e}")
 
 @app.websocket("/connect/{uuid}")
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
+    print(f"WebSocket conectado: {uuid}")
     connections[uuid] = websocket
     try:
         await handle_websocket_messages(websocket, uuid)
     except Exception as e:
-        print(f"Erro ao lidar com mensagens WebSocket: {e}")
+        print(f"Erro ao lidar com WebSocket {uuid}: {e}")
     finally:
+        print(f"WebSocket desconectado: {uuid}")
         del connections[uuid]
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
-    
-    
-    uuid = message.uuid_user  # O Xano retorna o UUID diretamente
-
-    # Verifique se temos uma conexão WebSocket para esse UUID
+    uuid = message.uuid_user
     if uuid in connections:
-        websocket = connections[uuid]
         try:
-            await websocket.send_text(message.mensagem)  # Envie a mensagem para o servidor local
-            response_message = await websocket.receive_text()  # Aguarde a resposta do servidor local
-            return {"response": response_message}
+            await message_queues[uuid].put(message.mensagem)
+            return {"response": "Mensagem enfileirada"}
         except Exception as e:
-            # Se ocorrer uma exceção ao tentar enviar/receber uma mensagem, assumimos que a conexão foi fechada
-            return {"response": "O Dexter não está sendo executado no seu servidor."}
+            return {"response": f"Erro ao enfileirar mensagem: {e}"}
     else:
-        return {"response": "O Dexter não está sendo executado no seu servidor."}
+        return {"response": "UUID não encontrado ou conexão não estabelecida"}
