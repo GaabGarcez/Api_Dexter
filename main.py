edb0ed07b5b0e1f1c7c338520890ca0e73f0cad4
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-20 21:52:56 -0300

    Versão 1.0 (Novo)

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 import asyncio
 import uuid
-import starlette
-import websockets
 
 app = FastAPI()
 
@@ -19,7 +17,7 @@ async def process_message(websocket: WebSocket, uuid: str):
     while True:
         if not message_queues[uuid].empty():
             message_id, message = await message_queues[uuid].get()
-            await websocket.send_text(f"{message_id}:{message}")  # Envio de message_id com a mensagem
+            await websocket.send_text(message)
             response = await websocket.receive_text()
             responses[message_id] = response  # Associa a resposta ao identificador da mensagem
         else:
@@ -31,17 +29,7 @@ async def websocket_endpoint(websocket: WebSocket, uuid: str):
     connections[uuid] = websocket
     if uuid not in message_queues:
         message_queues[uuid] = asyncio.Queue()
-
-    try:
-        await process_message(websocket, uuid)
-    except websockets.exceptions.ConnectionClosedOK:
-        # Remover a conexão do dicionário quando ela é fechada
-        connections.pop(uuid, None)
-    except starlette.websockets.WebSocketDisconnect as e:
-        # Lidar com desconexão WebSocket
-        print(f"WebSocket Disconnect Error: {e}")
-        connections.pop(uuid, None)
-
+    await process_message(websocket, uuid)
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
@@ -51,8 +39,6 @@ async def read_webhook(message: Message):
         await message_queues[target_uuid].put((message_id, message.mensagem))
         while message_id not in responses:
             await asyncio.sleep(0.1)
-        response = responses.pop(message_id)
-        response_content = response.split(":", 1)[1] if ":" in response else response  # Extrair apenas o conteúdo da resposta
-        return {"response": response_content}
+        return {"response": responses.pop(message_id)}  # Retorna a resposta associada ao identificador
     else:
         return {"response": "UUID não encontrado ou conexão não estabelecida"}
