ee501933be5d99d7ee23f7bf2ac0bf6217a3d6b3
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-11-23 20:43:21 -0300

    Versão 2.0

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
 from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 import asyncio
-import uuid
 
 app = FastAPI()
 
 connections = {}
-message_queues = {}
-responses = {}
+pending_messages = {}  # Inicializa o dicionário de mensagens pendentes
+message_events = {}
 
 class Message(BaseModel):
     uuid_user: str
     mensagem: str
 
-async def process_message(websocket: WebSocket, uuid: str):
+async def handle_websocket_messages(websocket: WebSocket, uuid: str):
     while True:
-        if not message_queues[uuid].empty():
-            message_id, message = await message_queues[uuid].get()
-            await websocket.send_text(message)
-            response = await websocket.receive_text()
-            responses[message_id] = response  # Associa a resposta ao identificador da mensagem
-        else:
-            await asyncio.sleep(0.1)
+        await message_events[uuid].wait()  # Espera até que uma mensagem esteja disponível
+        message = pending_messages.pop(uuid)
+        await websocket.send_text(message)
+        message_events[uuid].clear()  # Reseta o evento
 
 @app.websocket("/connect/{uuid}")
 async def websocket_endpoint(websocket: WebSocket, uuid: str):
     await websocket.accept()
     connections[uuid] = websocket
-    if uuid not in message_queues:
-        message_queues[uuid] = asyncio.Queue()
-    await process_message(websocket, uuid)
+    message_events[uuid] = asyncio.Event()  # Cria um novo Event para esta conexão
+    try:
+        await handle_websocket_messages(websocket, uuid)
+    except Exception as e:
+        print(f"Erro ao lidar com mensagens WebSocket: {e}")
+    finally:
+        del connections[uuid]
+        del message_events[uuid]  # Remove o evento associado
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
-    target_uuid = message.uuid_user
-    message_id = str(uuid.uuid4())  # Identificador único para a mensagem
-    if target_uuid in connections:
-        await message_queues[target_uuid].put((message_id, message.mensagem))
-        while message_id not in responses:
-            await asyncio.sleep(0.1)
-        return {"response": responses.pop(message_id)}  # Retorna a resposta associada ao identificador
+    uuid = message.uuid_user  
+    if uuid in connections:
+        websocket = connections[uuid]
+        try:
+            pending_messages[uuid] = message.mensagem
+            message_events[uuid].set()  # Sinaliza que uma nova mensagem está disponível
+            return {"response": "Mensagem enviada"}
+        except Exception as e:
+            return {"response": f"Erro ao enviar mensagem: {e}"}
     else:
         return {"response": "UUID não encontrado ou conexão não estabelecida"}
