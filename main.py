f6ecb04bfe2d4d5785ddc3e4f2acdf60cc72f991
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-10 17:33:20 -0300

    Versão 4.0

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI, WebSocket
 from pydantic import BaseModel
 import asyncio
 import uuid
+import logging
 
 app = FastAPI()
 
 connections = {}
 message_queues = {}
 responses = {}
+event_flags = {}
 
 class Message(BaseModel):
     uuid_user: str
@@ -20,8 +22,9 @@ async def handle_websocket_messages(websocket: WebSocket, uuid_user: str):
             await websocket.send_text(message_info['mensagem'])
             response_message = await websocket.receive_text()
             responses[message_info['message_id']] = response_message
+            event_flags[message_info['message_id']].set()
         else:
-            await asyncio.sleep(0.1)  # Pequeno atraso para evitar uso excessivo de CPU
+            await asyncio.sleep(0.1)
 
 @app.websocket("/connect/{uuid_user}")
 async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
@@ -31,7 +34,7 @@ async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
     try:
         await handle_websocket_messages(websocket, uuid_user)
     except Exception as e:
-        print(f"Erro ao lidar com mensagens WebSocket: {e}")
+        logging.error(f"Erro ao lidar com mensagens WebSocket: {e}")
     finally:
         del connections[uuid_user]
         del message_queues[uuid_user]
@@ -39,16 +42,16 @@ async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
 @app.post("/webhook/")
 async def read_webhook(message: Message):
     uuid_user = message.uuid_user
-    message_id = str(uuid.uuid4())  # Gerar um identificador único para cada mensagem
+    message_id = str(uuid.uuid4())
 
     if uuid_user in connections:
         if uuid_user not in message_queues:
             message_queues[uuid_user] = asyncio.Queue()
+        event_flags[message_id] = asyncio.Event()
         await message_queues[uuid_user].put({"mensagem": message.mensagem, "message_id": message_id})
         
         # Aguarda até que a resposta esteja disponível
-        while message_id not in responses:
-            await asyncio.sleep(0.1)
+        await event_flags[message_id].wait()
         return {"response": responses.pop(message_id)}
     else:
         return {"response": "O Dexter não está sendo executado no seu servidor."}
