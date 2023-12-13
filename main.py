e4379fab50dbaa4ea92cb5a33f5427b312c677c7
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-13 14:11:02 -0300

    Versão 7.3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, WebSocket
+from fastapi import WebSocketDisconnect
 from pydantic import BaseModel
 import asyncio
 import logging
@@ -28,14 +29,23 @@ class ConnectionManager:
             logging.info(f"Usuário {uuid_user} desconectado.")
 
     async def send_personal_message(self, message: str, uuid_user: str):
-        if uuid_user in self.active_connections:
-            await self.active_connections[uuid_user].send_text(message)
+        websocket = self.active_connections.get(uuid_user)
+        if websocket:
+            try:
+                await websocket.send_text(message)
+            except WebSocketDisconnect:
+                # Desconecte o usuário aqui, se necessário
+                await self.disconnect(uuid_user)
+                logging.info(f"Conexão com o usuário {uuid_user} perdida.")
+                return False
+        return True
 
     async def receive_personal_message(self, uuid_user: str):
         if uuid_user in self.active_connections:
             response_message = await self.active_connections[uuid_user].receive_text()
             self.responses[uuid_user] = response_message
             return response_message
+        
     async def is_connected(self, uuid_user: str):
         websocket = self.active_connections.get(uuid_user)
         return websocket and not websocket.closed
@@ -68,7 +78,8 @@ async def read_webhook(message: Message):
     uuid_user = message.uuid_user
     message_id = str(uuid.uuid4())
 
-    if await manager.is_connected(uuid_user):
+    # Verifique se a conexão está ativa antes de enviar a mensagem
+    if await manager.send_personal_message(message.mensagem, uuid_user):
         await manager.message_queues[uuid_user].put({"mensagem": message.mensagem, "message_id": message_id})
         while message_id not in manager.responses:
             await asyncio.sleep(0.1)
