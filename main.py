c5f007613e3d4766cb459f75b84aad435586e5ef
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-13 09:37:34 -0300

    Versão 5.1

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import uuid
 app = FastAPI()
 
 # Configuração de Logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)  # Alterado para DEBUG para mais detalhes
 
 connections = {}
 message_queues = {}
@@ -22,6 +22,7 @@ async def handle_websocket_messages(websocket: WebSocket, uuid_user: str):
         try:
             if uuid_user in message_queues and not message_queues[uuid_user].empty():
                 message_info = await message_queues[uuid_user].get()
+                logging.debug(f"Enviando mensagem para {uuid_user}: {message_info['mensagem']}")
                 await websocket.send_text(message_info['mensagem'])
                 response_message = await websocket.receive_text()
                 responses[message_info['message_id']] = response_message
@@ -36,13 +37,17 @@ async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
     await websocket.accept()
     connections[uuid_user] = websocket
     message_queues[uuid_user] = asyncio.Queue()
+    logging.info(f"Usuário {uuid_user} conectado.")
     try:
         await handle_websocket_messages(websocket, uuid_user)
     except Exception as e:
         logging.error(f"Erro na conexão WebSocket com usuário {uuid_user}: {e}")
     finally:
-        del connections[uuid_user]
-        del message_queues[uuid_user]
+        if uuid_user in connections:
+            del connections[uuid_user]
+            logging.info(f"Usuário {uuid_user} desconectado.")
+        if uuid_user in message_queues:
+            del message_queues[uuid_user]
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
@@ -55,8 +60,10 @@ async def read_webhook(message: Message):
         await message_queues[uuid_user].put({"mensagem": message.mensagem, "message_id": message_id})
         
         # Aguardar a resposta
+        logging.debug(f"Aguardando resposta para {message_id} de {uuid_user}")
         while message_id not in responses:
             await asyncio.sleep(0.1)
         return {"response": responses.pop(message_id)}
     else:
+        logging.warning(f"Tentativa de enviar mensagem para usuário {uuid_user}, mas não está conectado.")
         return {"response": "O Dexter não está sendo executado no seu servidor."}
