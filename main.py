d68a854ddf0272af6fab06f2665a51ac379019d3
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-14 14:22:44 -0300

    Versão 9.0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, WebSocket, HTTPException
-from starlette.websockets import WebSocketState
 from pydantic import BaseModel
 import asyncio
 import logging
@@ -17,34 +16,31 @@ class Message(BaseModel):
     mensagem: str
 
 async def handle_websocket_messages(websocket: WebSocket, uuid_user: str):
-    try:
-        while True:
+    while True:
+        try:
             if uuid_user in message_queues and not message_queues[uuid_user].empty():
                 message_info = await message_queues[uuid_user].get()
                 await websocket.send_text(message_info['mensagem'])
                 response_message = await websocket.receive_text()
                 responses[message_info['message_id']] = response_message
             else:
-                # Verifica se a conexão ainda está aberta
-                if not websocket.client_state == WebSocketState.CONNECTED:
-                    break
                 await asyncio.sleep(0.1)
-    except Exception as e:
-        logging.error(f"Erro na comunicação com o usuário {uuid_user}: {e}")
-    finally:
-        # Remove referências do usuário desconectado
-        connections.pop(uuid_user, None)
-        message_queues.pop(uuid_user, None)
-        responses.pop(uuid_user, None)
-        logging.info(f"Usuário {uuid_user} desconectado.")
+        except Exception as e:
+            logging.error(f"Erro na comunicação com o usuário {uuid_user}: {e}")
+            break  # Sair do loop em caso de erro
 
 @app.websocket("/connect/{uuid_user}")
 async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
     await websocket.accept()
     connections[uuid_user] = websocket
     message_queues[uuid_user] = asyncio.Queue()
-    logging.info(f"Usuário {uuid_user} conectado.")
-    await handle_websocket_messages(websocket, uuid_user)
+    try:
+        await handle_websocket_messages(websocket, uuid_user)
+    except Exception as e:
+        logging.error(f"Erro na conexão WebSocket com usuário {uuid_user}: {e}")
+    finally:
+        del connections[uuid_user]
+        del message_queues[uuid_user]
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
@@ -52,12 +48,15 @@ async def read_webhook(message: Message):
     message_id = str(uuid.uuid4())
 
     if uuid_user in connections:
+        if uuid_user not in message_queues:
+            message_queues[uuid_user] = asyncio.Queue()
         await message_queues[uuid_user].put({"mensagem": message.mensagem, "message_id": message_id})
+        
+        # Aguardar a resposta
         while message_id not in responses:
             await asyncio.sleep(0.1)
         return {"response": responses.pop(message_id)}
     else:
-        logging.warning(f"Tentativa de enviar mensagem para usuário {uuid_user}, mas não está conectado.")
         return {"response": "O Dexter não está sendo executado no seu servidor."}
 
 @app.post("/disconnect/{uuid_user}")
