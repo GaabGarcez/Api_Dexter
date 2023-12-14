de3b38706bb30363af7491a9e991a7f0f472aaa8
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-14 09:47:23 -0300

    Versão 8.0

--- main.py
+++ main.py
@@ -1,90 +1,73 @@
-from fastapi import FastAPI, WebSocket
-from fastapi import WebSocketDisconnect
-from websockets.exceptions import ConnectionClosedOK
+from fastapi import FastAPI, WebSocket, HTTPException
+from starlette.websockets import WebSocketState
 from pydantic import BaseModel
 import asyncio
 import logging
 import uuid
 
 app = FastAPI()
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)  # DEBUG para mais detalhes
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: dict[str, WebSocket] = {}
-        self.message_queues: dict[str, asyncio.Queue] = {}
-        self.responses: dict[str, str] = {}
-
-    async def connect(self, uuid_user: str, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections[uuid_user] = websocket
-        self.message_queues[uuid_user] = asyncio.Queue()
-        logging.info(f"Usuário {uuid_user} conectado.")
-
-    async def disconnect(self, uuid_user: str):
-        if uuid_user in self.active_connections:
-            await self.active_connections[uuid_user].close()
-            del self.active_connections[uuid_user]
-            del self.message_queues[uuid_user]
-            self.responses.pop(uuid_user, None)
-            logging.info(f"Usuário {uuid_user} desconectado.")
-
-    async def send_personal_message(self, message: str, uuid_user: str):
-        websocket = self.active_connections.get(uuid_user)
-        if websocket:
-            try:
-                await websocket.send_text(message)
-                return True
-            except (WebSocketDisconnect, ConnectionClosedOK):
-                # Desconecte o usuário aqui, se necessário
-                await self.disconnect(uuid_user)
-                logging.info(f"Conexão com o usuário {uuid_user} perdida.")
-                return False
-
-    async def receive_personal_message(self, uuid_user: str):
-        if uuid_user in self.active_connections:
-            response_message = await self.active_connections[uuid_user].receive_text()
-            self.responses[uuid_user] = response_message
-            return response_message
-        
-    async def is_connected(self, uuid_user: str):
-        websocket = self.active_connections.get(uuid_user)
-        return websocket and not websocket.closed
-
-manager = ConnectionManager()
+connections = {}
+message_queues = {}
+responses = {}
 
 class Message(BaseModel):
     uuid_user: str
     mensagem: str
 
-@app.websocket("/connect/{uuid_user}")
-async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
-    await manager.connect(uuid_user, websocket)
+async def handle_websocket_messages(websocket: WebSocket, uuid_user: str):
     try:
         while True:
-            if not manager.message_queues[uuid_user].empty():
-                message_info = await manager.message_queues[uuid_user].get()
-                await manager.send_personal_message(message_info['mensagem'], uuid_user)
-                response_message = await manager.receive_personal_message(uuid_user)
-                manager.responses[message_info['message_id']] = response_message
+            if uuid_user in message_queues and not message_queues[uuid_user].empty():
+                message_info = await message_queues[uuid_user].get()
+                await websocket.send_text(message_info['mensagem'])
+                response_message = await websocket.receive_text()
+                responses[message_info['message_id']] = response_message
             else:
+                # Verifica se a conexão ainda está aberta
+                if not websocket.client_state == WebSocketState.CONNECTED:
+                    break
                 await asyncio.sleep(0.1)
     except Exception as e:
         logging.error(f"Erro na comunicação com o usuário {uuid_user}: {e}")
     finally:
-        await manager.disconnect(uuid_user)
+        # Remove referências do usuário desconectado
+        connections.pop(uuid_user, None)
+        message_queues.pop(uuid_user, None)
+        responses.pop(uuid_user, None)
+        logging.info(f"Usuário {uuid_user} desconectado.")
+
+@app.websocket("/connect/{uuid_user}")
+async def websocket_endpoint(websocket: WebSocket, uuid_user: str):
+    await websocket.accept()
+    connections[uuid_user] = websocket
+    message_queues[uuid_user] = asyncio.Queue()
+    logging.info(f"Usuário {uuid_user} conectado.")
+    await handle_websocket_messages(websocket, uuid_user)
 
 @app.post("/webhook/")
 async def read_webhook(message: Message):
     uuid_user = message.uuid_user
     message_id = str(uuid.uuid4())
 
-    # Verifique se a conexão está ativa antes de enviar a mensagem
-    if await manager.send_personal_message(message.mensagem, uuid_user):
-        await manager.message_queues[uuid_user].put({"mensagem": message.mensagem, "message_id": message_id})
-        while message_id not in manager.responses:
+    if uuid_user in connections:
+        await message_queues[uuid_user].put({"mensagem": message.mensagem, "message_id": message_id})
+        while message_id not in responses:
             await asyncio.sleep(0.1)
-        return {"response": manager.responses.pop(message_id)}
+        return {"response": responses.pop(message_id)}
     else:
-        logging.warning(f"Tentativa de enviar mensagem para usuário {uuid_user}, mas o Dexter está desligado.")
+        logging.warning(f"Tentativa de enviar mensagem para usuário {uuid_user}, mas não está conectado.")
         return {"response": "O Dexter não está sendo executado no seu servidor."}
+
+@app.post("/disconnect/{uuid_user}")
+async def disconnect_client(uuid_user: str):
+    if uuid_user in connections:
+        await connections[uuid_user].close()
+        connections.pop(uuid_user, None)
+        message_queues.pop(uuid_user, None)
+        responses.pop(uuid_user, None)
+        logging.info(f"Usuário {uuid_user} desconectado.")
+        return {"status": "Disconnected"}
+    else:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
