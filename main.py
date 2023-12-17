d155770b7e7985626623b036f3ce25273c09dcf4
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-17 18:28:44 -0300

    Teste

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
-from fastapi import FastAPI
-from fastapi_socketio import SocketManager
-from pydantic import BaseModel
-import asyncio
-import logging
-import uuid
+import socketio
+from fastapi import FastAPI, Request
 
+sio = socketio.AsyncServer(async_mode='asgi')
 app = FastAPI()
-sio = SocketManager(app=app)
-logging.basicConfig(level=logging.DEBUG)
+sio.attach(app)
 
-connections = {}  # Mapeia UUIDs de usuários para SIDs do Socket.IO
-pending_responses = {}  # Mapeia message_ids para futuros de resposta
+connections = {}  # Dicionário para mapear IDs personalizados para SIDs do Socket.IO
 
-class Message(BaseModel):
-    uuid_user: str
-    mensagem: str
+@sio.event
+async def connect(sid, environ):
+    print(f'Cliente conectado com SID: {sid}')
 
-@app.post("/webhook/")
-async def read_webhook(message: Message):
-    uuid_user = message.uuid_user
-    message_id = str(uuid.uuid4())
+@sio.event
+async def disconnect(sid):
+    print(f'Cliente desconectado com SID: {sid}')
+    # Remove o SID do mapeamento ao desconectar
+    for key, value in list(connections.items()):
+        if value == sid:
+            del connections[key]
 
-    if uuid_user in connections:
-        response_future = asyncio.get_event_loop().create_future()
-        pending_responses[message_id] = response_future
-        await sio.emit('message_event', {"mensagem": message.mensagem, "message_id": message_id}, to=connections[uuid_user])
+@sio.event
+async def registrar_id(sid, data):
+    meu_id = data['meu_id']
+    connections[meu_id] = sid  # Mapeia o ID personalizado para o SID
+    print(f'O ID personalizado do cliente {sid} é {meu_id}')
 
-        try:
-            response = await asyncio.wait_for(response_future, timeout=30)  # Aguarda a resposta por 30 segundos
-            return {"response": response}
-        except asyncio.TimeoutError:
-            return {"response": "Timeout na espera pela resposta."}
-    else:
-        return {"response": "Usuário não conectado ou não encontrado."}
+@app.get("/clientes_conectados")
+async def get_connected_clients(request: Request):
+    return connections
 
-@sio.on('message_response')
-async def handle_message_response(sid, data):
-    message_id = data['message_id']
-    if message_id in pending_responses:
-        pending_responses[message_id].set_result(data['resposta'])
-        del pending_responses[message_id]
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
