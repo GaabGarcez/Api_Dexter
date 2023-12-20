e37e17a66e19e7022cc2daf5758b946841527bcc
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-19 23:08:37 -0300

    Versão Final 1.0

--- main.py
+++ main.py
@@ -1,84 +1,24 @@
-from fastapi import FastAPI
-import threading
-from autobahn.twisted.websocket import WebSocketServerProtocol, WebSocketServerFactory
-from twisted.internet import reactor
-import json
-import logging
-from pydantic import BaseModel
-import logging
-import os
+from fastapi import FastAPI, HTTPException
+import requests
 
 app = FastAPI()
-class Message(BaseModel):
-    uuid_user: str
-    mensagem: str
-# Fábrica do Servidor WebSocket
-class MeuServidorFactory(WebSocketServerFactory):
+connections = {}
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.clients = {}
+@app.post("/connect/")
+async def connect(uuid_user: str, ngrok_url: str):
+    connections[uuid_user] = ngrok_url
+    return {"message": "Conexão registrada"}
 
-    def register(self, client, client_id):
-        self.clients[client_id] = client
-        logging.info(f"Cliente registrado: ID = {client_id}, Total de clientes = {len(self.clients)}")
+@app.post("/disconnect/")
+async def disconnect(uuid_user: str):
+    connections.pop(uuid_user, None)
+    return {"message": "Conexão removida"}
 
-
-    def unregister(self, client_id):
-        if client_id in self.clients:
-            del self.clients[client_id]
-            logging.info(f"Cliente {client_id} desregistrado.")
-
-    def encaminhar_mensagem_para_cliente(self, id_cliente, mensagem):
-        if id_cliente in self.clients:
-            cliente = self.clients[id_cliente]
-            cliente.sendMessage(json.dumps(mensagem).encode('utf8'))
-            logging.info(f"Mensagem encaminhada para {id_cliente}.")
-            return True
-        else:
-            logging.info(f"Nenhum cliente conectado com o ID {id_cliente}.")
-            return False
-
-# Protocolo do Servidor WebSocket
-class MeuServidorProtocolo(WebSocketServerProtocol):
-
-    def onOpen(self):
-        logging.info("Conexão WebSocket aberta.")
-
-    def onMessage(self, payload, isBinary):
-        if not isBinary:
-            mensagem = json.loads(payload.decode('utf8'))
-
-            if mensagem.get("tipo") == "id":
-                self.uuid_user = mensagem['id']
-                self.factory.register(self, self.uuid_user)
-                logging.info(f"ID único recebido: {self.uuid_user}")
-            else:
-                # Outras mensagens podem ser tratadas aqui
-                pass
-
-    def onClose(self, wasClean, code, reason):
-        if hasattr(self, 'uuid_user'):
-            self.factory.unregister(self.uuid_user)
-            logging.info(f"Conexão WebSocket fechada para {self.uuid_user}: {reason}")
-
-# Inicialização do servidor WebSocket
-@app.on_event("startup")
-def start_websocket_server():
-    logging.basicConfig(level=logging.INFO)
-    global factory
-    factory = MeuServidorFactory()  # Endereço removido
-    factory.protocol = MeuServidorProtocolo
-    port = int(os.environ.get("PORT", 80))
-    reactor.listenTCP(port, factory)
-
-
-@app.post("/webhook/")
-async def read_webhook(message: Message):
-    uuid_user = message.uuid_user
-    mensagem_user = message.mensagem
-
-    if factory.encaminhar_mensagem_para_cliente(uuid_user, mensagem_user):
-        return {"status": f"Mensagem enviada para o cliente {uuid_user}, com o conteúdo: {mensagem_user}"}
-    else:
-        return {"status": "Nenhum cliente conectado com o ID fornecido."}
+@app.post("/request/")
+async def make_request(uuid_user: str, mensagem: str):
+    if uuid_user not in connections:
+        return "Dextr não está em funcionamento neste servidor"
+    
+    ngrok_url = connections[uuid_user]
+    response = requests.post(f"{ngrok_url}/webhook/", json={"mensagem": mensagem})
+    return response.json()
