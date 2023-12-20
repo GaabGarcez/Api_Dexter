8372a17c5f49041faa30c8259ceedb716c1d585c
Antonio Gabriel Garcez <antoniogabrielgarcezbarbosa@gmail.com>
2023-12-19 23:41:28 -0300

    Versão Final 3.0

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel
 app = FastAPI()
 connections = {}
 
-
 class RequestData(BaseModel):
     uuid_user: str
     mensagem: str
@@ -13,7 +12,7 @@ class RequestData(BaseModel):
 @app.post("/request/")
 async def make_request(data: RequestData):
     if data.uuid_user not in connections:
-        return "Dextr não está em funcionamento neste servidor"
+        return "Dexter não está em funcionamento neste servidor"
     
     ngrok_url = connections[data.uuid_user]
     response = requests.post(f"{ngrok_url}/webhook/", json={"mensagem": data.mensagem})
@@ -22,9 +21,10 @@ async def make_request(data: RequestData):
 @app.post("/connect/")
 async def connect(uuid_user: str, ngrok_url: str):
     connections[uuid_user] = ngrok_url
-    return {"message": "Conexão registrada"}
+    print(f"Conectado: {uuid_user}, URL: {ngrok_url}")
 
 @app.post("/disconnect/")
 async def disconnect(uuid_user: str):
-    connections.pop(uuid_user, None)
-    return {"message": "Conexão removida"}
+    if uuid_user in connections:
+        print(f"Desconectado: {uuid_user}")
+        connections.pop(uuid_user)
